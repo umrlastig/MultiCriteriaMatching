12b762c1109e9a0a0eb37936203fcf716b71511c
mdvandamme <marie-dominique.vandamme@ign.fr>
2021-12-11 18:10:32 +0100

    Wu Palmer

---
+++ src/test/java/fr/ign/cogit/ontology/TestOntoText.java
@@ -0,0 +1,38 @@
+package fr.ign.cogit.ontology;
+
+public class TestOntoText {
+
+	private void loadA() {
+		//
+//				enfantParents = new HashMap<String, List<String>>();
+//				enfantParents.put("A", Arrays.asList("B", "F", "L"));
+//				enfantParents.put("B", Arrays.asList("C"));
+//				enfantParents.put("C", Arrays.asList("D"));
+//				enfantParents.put("D", Arrays.asList("E"));
+//				enfantParents.put("E", Arrays.asList("Z"));
+//				
+//				enfantParents.put("F", Arrays.asList("G", "I"));
+//				enfantParents.put("G", Arrays.asList("H"));
+//				enfantParents.put("H", Arrays.asList("Z"));
+//				
+//				enfantParents.put("I", Arrays.asList("J"));
+//				enfantParents.put("J", Arrays.asList("K"));
+//				enfantParents.put("K", Arrays.asList("Z"));
+//				
+//				enfantParents.put("L", Arrays.asList("M"));
+//				enfantParents.put("M", Arrays.asList("N", "P"));
+//				enfantParents.put("N", Arrays.asList("O"));
+//				enfantParents.put("O", Arrays.asList("Z"));
+//				
+//				enfantParents.put("P", Arrays.asList("Z"));
+//				
+//				enfantParents.put("Q", Arrays.asList("R"));
+//				enfantParents.put("R", Arrays.asList("Z"));
+//				
+//				enfantParents.put("S", Arrays.asList("T"));
+//				enfantParents.put("T", Arrays.asList("U", "V"));
+//				enfantParents.put("U", Arrays.asList("Z"));
+//				enfantParents.put("V", Arrays.asList("Z"));
+			}
+		
+}
